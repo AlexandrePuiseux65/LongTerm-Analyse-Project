583fb35ee6ec8c4f674c282cfe4c5e04c03ffb60
Alexandre Puiseux <122177445+AlexandrePuiseux65@users.noreply.github.com>
2026-01-28 19:35:57 +0000

    verif yahoo fiance send element not empty

--- function/services/finance.py
+++ function/services/finance.py
@@ -1,20 +1,25 @@
 from function.services.fetch import FetchDataStocks
 from function.domain.stock import Stock
 
+# factory fonction
 def load_stock(ticker: str):
-    fetch = FetchDataStocks(ticker)
+    try : 
+        fetch = FetchDataStocks(ticker)
 
-    return Stock(
-        ticker=ticker,
-        prices=fetch.get_price_history(),
-        current_price=fetch.get_current_price(),
-        market_cap=fetch.get_market_cap(),
-        income_statement=fetch.get_income_statement(),
-        cashflow=fetch.get_cashflow(),
-        balance_sheet=fetch.get_balance_sheet(),
-        dividends=fetch.get_dividends(),
-        dividents_all= fetch.get_annual_dividend(),
-        shares_outstanding=fetch.get_shares_outstanding(),
-        news=fetch.get_news()
-    )
+        return Stock(
+            ticker=ticker,
+            prices=fetch.get_price_history(),
+            current_price=fetch.get_current_price(),
+            market_cap=fetch.get_market_cap(),
+            income_statement=fetch.get_income_statement(),
+            cashflow=fetch.get_cashflow(),
+            balance_sheet=fetch.get_balance_sheet(),
+            dividends=fetch.get_dividends(),
+            dividents_all= fetch.get_annual_dividend(),
+            shares_outstanding=fetch.get_shares_outstanding(),
+            news=fetch.get_news()
+        )
+    except Exception as e: 
+        print("Error: N/A data collected from yahoo finance.")
+        return None
 
